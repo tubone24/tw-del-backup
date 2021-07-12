1b8e135a6849137d0d68d23f045cbff9a6629011
tubone24 <tubo.yyyuuu@gmail.com>
2021-07-12 16:52:27 +0900

    fix

--- src/decrypt_backup.py
+++ src/decrypt_backup.py
@@ -1,7 +1,9 @@
-import os
+import sys
 from encrypt import AESCipher
-BACKUP_KEY = os.getenv("BACKUP_KEY")
+BACKUP_KEY = sys.argv[1]
+FILEPATH = sys.argv[2]
+
 
 aes = AESCipher(key=BACKUP_KEY)
 
-aes.decrypt_file("backup.json.enc", delete_raw_file=False)
+aes.decrypt_file(FILEPATH, delete_raw_file=False)
