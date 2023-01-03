a1fcaf9ac2bcc9bf2a18ae888994ffa8c7bb4e8a
tubone <meitantei-conan@live.jp>
2023-01-03 04:21:34 +0000

    fix

--- src/decrypt_media.py
+++ src/decrypt_media.py
@@ -1,6 +1,7 @@
+import sys
 from encrypt import AESCipher
 BACKUP_KEY = sys.argv[1]
 FILEPATH = sys.argv[2]
 
 aes = AESCipher(key=BACKUP_KEY)
-aes.decrypt_bytes(FILEPATH + ".enc", delete_raw_file=True)
+aes.decrypt_bytes(FILEPATH, delete_raw_file=False)
